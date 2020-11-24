99b7c89dfd04c199df79f1a4f1df0208efffce10
erri120 <erri120@protonmail.com>
2020-11-24 17:38:33 +0100

    Create some dummy projects

---
+++ FunWithGithubActions.Test/ProgramTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace FunWithGithubActions.Test
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            const int a = 2;
+            const int b = 4;
+            const int expected = 6;
+            
+            var actual = Program.Add(a, b);
+            
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ FunWithGithubActions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FunWithGithubActions
+{
+    public static class Program
+    {
+        public static int Main()
+        {
+            Console.WriteLine("Input a:");
+            var line = Console.ReadLine();
+            if (!int.TryParse(line, out var a))
+            {
+                Console.WriteLine($"\"{line}\" is not parseable!");
+                return -1;
+            }
+            
+            Console.WriteLine("Input b:");
+            line = Console.ReadLine();
+            if (!int.TryParse(line, out var b))
+            {
+                Console.WriteLine($"\"{line}\" is not parseable!");
+                return -1;
+            }
+
+            var result = Add(a, b);
+            Console.WriteLine($"{a} + {b} = {Add(a, b)}");
+
+            return result;
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
